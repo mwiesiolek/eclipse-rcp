058ad894ad9ef6f762b657ded59f49b691237157
Maksymilian Wiesiolek <m.wiesiolek@globallogic.com>
2019-04-13 14:31:07 +0200

    enablement of handlers

--- com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/ExitHandler.java
+++ com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/ExitHandler.java
@@ -2,14 +2,22 @@ package com.vogella.tasks.ui.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
 public class ExitHandler {
 	@Execute
-	public void execute(IWorkbench workbench, Shell shell) {
-        boolean result = MessageDialog.openConfirm(shell, "Close",
-                "Close application?");
+	public void execute(IWorkbench workbench, Shell shell, EPartService partService) {
+		if(!partService.getDirtyParts().isEmpty()) {
+			boolean result = MessageDialog.openConfirm(shell, "Config", "Unsaved changes have been detected. Do you want to discard them?");
+			if(result) {
+				workbench.close();
+				return;
+			}
+		}
+		
+        boolean result = MessageDialog.openConfirm(shell, "Close", "Close application?");
         if (result) {
             workbench.close();
         }
--- com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/SaveAllHandler.java
+++ com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/SaveAllHandler.java
@@ -1,11 +1,18 @@
 package com.vogella.tasks.ui.handlers;
 
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
 public class SaveAllHandler {
-    @Execute
+    
+	@Execute
     void execute(EPartService partService) {
         partService.saveAll(false);
     }
+    
+	@CanExecute
+	boolean canExecute(EPartService partService) {
+		return !partService.getDirtyParts().isEmpty();
+	}
 }
